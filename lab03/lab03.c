ae9678f666172e1f1194cb53668397ac7bbfac59
Ian <101913171+IanMcunha@users.noreply.github.com>
2024-03-18 21:17:32 -0300

    Update lab03.c

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -7,40 +7,42 @@
 #include <sched.h>
 #include <stdio.h>
 
-// 64kB stack
+// Define o tamanho da pilha da nova thread como 64kB.
 #define FIBER_STACK 1024*64
 
-// The child thread will execute this function
+// Função que será executada pela nova thread.
 int threadFunction(void* argument) {
-    printf("Child thread entering\n");
+    printf("Thread filha iniciando\n");
 
-    // O argumento é um ponteiro para um valor inteiro compartilhado com o processo pai
+    // O argumento é um ponteiro para um valor inteiro compartilhado com o processo pai.
     int* shared_value = (int*)argument;
 
-    // Incrementa o valor compartilhado
+    // Incrementa o valor compartilhado.
     *shared_value += 1;
 
-    printf("Child thread incrementing shared value to %d\n", *shared_value);
+    printf("Thread filha incrementando o valor compartilhado para %d\n", *shared_value);
 
-    printf("Child thread exiting\n");
+    printf("Thread filha encerrando\n");
     return 0;
 }
 
 int main() {
     void* stack;
     pid_t pid;
-    int shared_value = 0; // Valor inicial compartilhado com a thread
+    // Valor inicial compartilhado com a thread.
+    int shared_value = 0;
 
-    // Allocate the stack
+    // Aloca memória para a pilha da nova thread.
     stack = malloc(FIBER_STACK);
     if (stack == 0) {
         perror("malloc: could not allocate stack");
         exit(1);
     }
 
-    printf("Creating child thread with shared value %d\n", shared_value);
+    // Criação da thread filha com valor compartilhado inicializado.
+    printf("Criando thread filha com valor compartilhado %d\n", shared_value);
 
-    // Call the clone system call to create the child thread
+    // Chama a função clone para criar a thread filha, especificando as flags para compartilhamento de recursos.
     pid = clone(&threadFunction, (char*)stack + FIBER_STACK,
                 SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, &shared_value);
     if (pid == -1) {
@@ -48,19 +50,19 @@ int main() {
         exit(2);
     }
 
-    // Wait for the child thread to exit
+    // Espera pela conclusão da thread filha.
     pid = waitpid(pid, 0, 0);
     if (pid == -1) {
         perror("waitpid");
         exit(3);
     }
 
-    // Print the shared value after the thread has exited
-    printf("Parent process: Shared value is now %d\n", shared_value);
+    // Imprime o valor compartilhado após a execução da thread filha.
+    printf("Processo pai: O valor compartilhado agora é %d\n", shared_value);
 
-    // Free the stack
+    // Libera a memória alocada para a pilha da thread filha.
     free(stack);
-    printf("Child thread returned and stack freed.\n");
+    printf("Thread filha retornou e a pilha foi liberada\n");
 
     return 0;
 }
