9b89c8cd429d93c71cf8043e3511049fd6b84052
Ian <101913171+IanMcunha@users.noreply.github.com>
2024-04-19 22:23:15 -0300

    Update projProcessos.c

--- proj1/projProcessos.c
+++ proj1/projProcessos.c
@@ -1,80 +1,48 @@
 #include <stdio.h>
-#include <pthread.h>
-#define MAXN 10000
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
 
-int saida = 0;
+#define MAXN 10000
 
 typedef struct {
     int tempo;
     int direcao;
 } Pessoa;
 
-Pessoa filaSubida[MAXN];
-int tamanhoFS = 0;
-Pessoa filaDescida[MAXN];
-int tamanhoFD = 0;
+Pessoa filaSubir[MAXN];
+int tamanhoFS;
+Pessoa filaDescer[MAXN];
+int tamanhoFD;
 Pessoa primeiro;
 
-void* escalador() {
+int escadaRolante() {
 
-    int indiceFS = 0, indiceFD = 0;
-
-    while (indiceFS < tamanhoFS || indiceFD < tamanhoFD) {
-
-        // subindo
+    int aux1 = 0, aux2 = 0, saida = 0;
+    while (aux1 < tamanhoFS || aux2 < tamanhoFD) {
         if (primeiro.direcao == 0) {
-
-            // segue a mesma direção se o tempo de chegada for menor que o último tempo de saída ou se estiver imediatamente ao lado do anterior
-            if ((indiceFS < tamanhoFS && (filaSubida[indiceFS].tempo <= saida) || (filaSubida[indiceFS].tempo > saida && filaSubida[indiceFS].tempo < filaDescida[indiceFD].tempo)) || indiceFD == tamanhoFD) {
-                primeiro = filaSubida[indiceFS];               
-                ++indiceFS;   
-
-            // se o tempo de chegada for maior que o tempo de saída ou a fila estiver completamente usada 
-            } else if (filaSubida[indiceFS].tempo > saida || indiceFS == tamanhoFS) {
-                // muda a direção do escalador, considerando um novo tempo de chegada baseado no tempo de espera
-                primeiro = filaDescida[indiceFD];
-                if (saida > primeiro.tempo) primeiro.tempo = saida;
-                ++indiceFD;
-
-                // se o tempo de saída for maior que os seguintes
-                int i = indiceFD;
-                while (saida > filaDescida[i].tempo && i < tamanhoFD) {
-                    filaDescida[i].tempo = saida;
-                    ++i;
-                }
-            } 
-        // descendo                              
-        } else if (primeiro.direcao == 1) {
-
-            // segue a mesma direção se o tempo de chegada for menor que o último tempo de saída ou se estiver imediatamente ao lado do anterior
-            if ((indiceFD < tamanhoFD && filaDescida[indiceFD].tempo <= saida || (filaDescida[indiceFD].tempo > saida && filaDescida[indiceFD].tempo < filaSubida[indiceFS].tempo)) || indiceFS == tamanhoFS) {
-                primeiro = filaDescida[indiceFD];      
-                ++indiceFD;
-
-            // se o tempo de chegada for maior que o tempo de saída ou a fila estiver completamente usada                                               
-            } else if (filaDescida[indiceFD].tempo > saida || indiceFD == tamanhoFD) {
-                // muda a direção do escalador, considerando um novo tempo de chegada baseado no tempo de espera
-                primeiro = filaSubida[indiceFS];
-                if (saida > primeiro.tempo) primeiro.tempo = saida;
-                ++indiceFS;
-
-                // se o tempo de saída for maior que os seguintes
-                int i = indiceFS;
-                while (saida > filaSubida[i].tempo && i < tamanhoFS) {
-                    filaSubida[i].tempo = saida;
-                    ++i;
-                }                
+            if ((aux1 < tamanhoFS && filaSubir[aux1].tempo <= saida) || (aux1 < tamanhoFS && filaSubir[aux1].tempo > saida && aux1 + 1 < tamanhoFS && filaSubir[aux1 + 1].tempo > saida) || aux2 >= tamanhoFD) {
+                primeiro = filaSubir[aux1];
+                ++aux1;
+            } else if (aux2 < tamanhoFD) {
+                primeiro = filaDescer[aux2];
             }
-        }   
+        } else {
+            if ((aux2 < tamanhoFD && filaDescer[aux2].tempo <= saida) || (aux2 < tamanhoFD && filaDescer[aux2].tempo > saida && aux2 + 1 < tamanhoFD && filaDescer[aux2 + 1].tempo > saida) || aux1 >= tamanhoFS) {
+                primeiro = filaDescer[aux2];
+                ++aux2;
+            } else if (aux1 < tamanhoFS) {
+                primeiro = filaSubir[aux1];
+            }
+        }
         saida = primeiro.tempo + 10;
     }
-    return NULL;
+    return saida;
 }
 
-
 int main() {
-    pthread_t idThread;
-
     char caminhoArquivo[256];
     printf("Digite o caminho do arquivo de entrada (ex: ./input/E_58.txt): ");
     scanf("%s", caminhoArquivo);
@@ -95,17 +63,15 @@ int main() {
           primeiro = p;
         }
         if (p.direcao == 0) {
-            filaSubida[tamanhoFS++] = p;
+            filaSubir[tamanhoFS++] = p;
         } else {
-            filaDescida[tamanhoFD++] = p;
+            filaDescer[tamanhoFD++] = p;
         }
     }
-
     fclose(arquivo);
 
-    pthread_create(&idThread, NULL, escalador, NULL);
-    pthread_join(idThread, NULL);
-
+    int saida = escadaRolante();  // Chamada direta à função escadaRolante sem necessidade de processo adicional ou mmap
     printf("Saida: %d\n", saida);
+
     return 0;
 }
