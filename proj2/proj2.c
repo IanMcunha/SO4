ffe5f2435928b40161dd12e42e58e14bfe6e9bdc
Ian <101913171+IanMcunha@users.noreply.github.com>
2024-05-17 23:01:41 -0300

    Update proj2.c

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -1,82 +1,107 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <malloc.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sched.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-// 64kB stack
-#define FIBER_STACK 1024*64
-
-struct c {
+struct conta {
+    char nome[10];
     int saldo;
 };
-typedef struct c conta;
+typedef struct conta conta;
 
-conta from, to;
-int valor;
-pthread_mutex_t mutex;
+struct transacao {
+    conta *recebe;
+    conta *envia;
+    int valor;
+    int *contador; // Adicionando um ponteiro para o contador de transações bem-sucedidas
+};
+typedef struct transacao transacao;
 
-// The child thread will execute this function
-int transferencia(void *arg) {
-    pthread_mutex_lock(&mutex);
-    if (from.saldo >= valor) {
-        from.saldo -= valor;
-        to.saldo += valor;
+conta conta1;
+conta conta2;
+pthread_mutex_t semaforo;
+
+void *transferir(void *args) {
+    transacao *t = (transacao *)args;
+    pthread_mutex_lock(&semaforo);
+
+    printf("---------------------------------------\n");
+    if (t->envia->saldo - t->valor >= 0) {
+        printf("Transação de %s para %s\n", t->envia->nome, t->recebe->nome);
+        printf("Valor da transação: $%d,00\n", t->valor);
+
+        printf("%s - Saldo Anterior: $%d,00\n", t->envia->nome, t->envia->saldo);
+        t->envia->saldo = t->envia->saldo - t->valor;
+        printf("%s - Saldo Posterior: $%d,00\n", t->envia->nome, t->envia->saldo);
+
+        printf("%s - Saldo Anterior: $%d,00\n", t->recebe->nome, t->recebe->saldo);
+        t->recebe->saldo = t->recebe->saldo + t->valor;
+        printf("%s - Saldo Posterior: $%d,00\n", t->recebe->nome, t->recebe->saldo);
+
+        // Incrementa a contagem de transações bem-sucedidas
+        (*(t->contador))++;
+    } else {
+        printf("Saldo Insuficiente\n");
     }
-    printf("Transferência concluída com sucesso!\n");
-    printf("Saldo de c1: %d\n", from.saldo);
-    printf("Saldo de c2: %d\n", to.saldo);
-    pthread_mutex_unlock(&mutex);
-    return 0;
+
+    pthread_mutex_unlock(&semaforo);
+    free(t);
+    return NULL;
 }
 
-int main() {
-    void* stack;
-    pid_t pid;
-    int i;
+int main(void) {
+    srand(time(NULL));
+    strcpy(conta1.nome, "Conta 1");
+    conta1.saldo = 1000;
+    strcpy(conta2.nome, "Conta 2");
+    conta2.saldo = 1000;
 
-    // Initialize the mutex
-    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&semaforo, NULL);
 
-    // Allocate the stack
-    stack = malloc(FIBER_STACK);
-    if (stack == 0) {
-        perror("malloc: could not allocate stack");
-        exit(1);
-    }
+    int totalDeTransacoes = 10;
+    int transacoes_sucesso = 0; // Contador de transações bem-sucedidas
+    int totalDeLoops = totalDeTransacoes / 100 + (totalDeTransacoes % 100 != 0);
+    int count = 0;
 
-    // Todas as contas começam com saldo 100
-    from.saldo = 100;
-    to.saldo = 100;
+    for (int i = 0; i < totalDeLoops; i++) {
+        pthread_t thread_id[100];
 
-    printf("Transferindo 10 para a conta c2\n");
-    valor = 10;
+        for (int j = 0; j < 100; j++) {
+            if (count >= totalDeTransacoes) {
+                break;
+            }
 
-    for (i = 0; i < 10; i++) {
-        // Call the clone system call to create the child thread
-        pid = clone(&transferencia, (char*)stack + FIBER_STACK,
-            SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0);
-        if (pid == -1) {
-            perror("clone");
-            exit(2);
+            transacao *t = (transacao *)malloc(sizeof(transacao));
+            int n = rand() % 2;
+            if (n == 0) {
+                t->envia = &conta1;
+                t->recebe = &conta2;
+            } else {
+                t->envia = &conta2;
+                t->recebe = &conta1;
+            }
+            t->valor = rand() % 500 + 1;
+            t->contador = &transacoes_sucesso; // Passa o ponteiro do contador
+
+            pthread_create(&thread_id[j], NULL, transferir, (void *)t);
+            count++;
+        }
+
+        for (int j = 0; j < 100; j++) {
+            if (j < count) {
+                pthread_join(thread_id[j], NULL);
+            }
         }
     }
 
-    // Wait for all child processes to complete
-    for (i = 0; i < 10; i++) {
-        wait(NULL);
-    }
+    pthread_mutex_destroy(&semaforo);
 
-    // Free the stack
-    free(stack);
-    printf("Transferências concluídas e memória liberada.\n");
-
-    // Destroy the mutex
-    pthread_mutex_destroy(&mutex);
+    printf("---------------------------------------\n");
+    printf("Total de Transações Tentadas: %d\n", totalDeTransacoes);
+    printf("Total de Transações Bem-sucedidas: %d\n", transacoes_sucesso);
+    printf("Saldo final da %s: $%d,00\n", conta1.nome, conta1.saldo);
+    printf("Saldo final da %s: $%d,00\n", conta2.nome, conta2.saldo);
 
     return 0;
 }
